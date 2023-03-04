5851cb97721013be81f2488c3770e56f44d8c29d
Salma <77203008+salmakhaled74@users.noreply.github.com>
2023-03-05 00:25:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,55 @@
 const express = require('express');
-const qrcode = require('qrcode');
+const jsQR = require("jsqr");
 const path = require('path');
+const qrcode = require('qrcode');
 const bodyParser = require('body-parser');
-
+const { createCanvas } = require('canvas');
 const app = express();
 
+app.use(express.json({ limit: '10mb' }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/', (req, res) => {
-  const { url } = req.body;
-  const options = {
-    width: 160,
-    height: 160
-  };
-  qrcode.toDataURL(url, options, (err, qrCodeUrl) => {
-    if (err) {
-      return res.status(500).send('Error generating QR code');
+app.post('/scan', (req, res) => {
+    console.log('post succeeded');
+    const { width, height, data } = req.body;
+    const canvas = createCanvas(width, height);
+    const ctx = canvas.getContext('2d');
+    const imageData = ctx.createImageData(width, height);
+    imageData.data.set(data);
+    ctx.putImageData(imageData, 0, 0);
+    const code = jsQR(imageData.data, imageData.width, imageData.height);
+    if (code) {
+        console.log("Found QR code");
+        const shortLink = code.data.substring(0, 5);
+        res.send({ codeData: code.data });
+        if (shortLink === 'https') {
+            console.log(code.data);
+        } else {
+            console.log(shortLink);
+            console.log('The link is not safe');
+        }
+    } else {
+        console.log('Please enter a valid Qr code image');
+        res.sendStatus(400);
     }
-    res.json({ qrCodeUrl });
-  });
 });
 
+app.post('/generate', (req, res) => {
+    const { url } = req.body;
+    const options = {
+        width: 160,
+        height: 160
+    };
+    qrcode.toDataURL(url, options, (err, qrCodeUrl) => {
+        if (err) {
+            return res.status(500).send('Error generating QR code');
+        }
+        res.json({ qrCodeUrl });
+    });
+});
 
-
-// start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
